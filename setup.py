fadf856072a3067c982b119d8d8b40797a283dda
Sean Brar <seanbrar@yahoo.com>
2024-09-12 03:38:15 -0700

    increment version to v0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="paperweight",
-    version="0.1.0",
+    version="0.1.1",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
